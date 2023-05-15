186e95c46808fe4cf320a30aaa541e4fc8eee1c5
rashadheydarli <rashadahe@code.edu.az>
2023-05-15 13:51:32 +0400

    2nd commit

---
+++ scripts/giftcards.js
@@ -0,0 +1,14 @@
+
+
+// $(document).ready(function(){
+//     $('.your-class').slick({
+//       setting-name: setting-value
+//     });
+//   });
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
